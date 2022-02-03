3f9d27801f697229548dd36caf0b13d00976c429
Jose Otero <joseotero@Joses-MBP.attlocal.net>
2022-02-02 21:27:39 -0500

    first commit

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,74 @@
-// HINT: You can delete this console.log after you no longer need it!
-console.log('JavaScript code has loaded!')
-// First, tell us your name
-let yourName = "Jane Doe" // HINT: Replace this with your own name!
+let yourName = "Jose Otero";
 
-// We'll use these variables to track the counts of each cookie type
-let gb = 0 // Gingerbread
-let cc = 0 // Chocolate Chip
-let sugar = 0 // Sugar Sprinkle
+// Cookie type
+let gb = 0; // Gingerbread
+let cc = 0; // Chocolate Chip
+let sugar = 0; // Sugar Sprinkle
 
 // selecting the element with an id of credit
-const credit = document.querySelector('#credit')
-// selecting the element with an id of add-gb
-const gbPlusBtn = document.querySelector('#add-gb')
+const credit = document.querySelector("#credit");
+// selecting the element with an id of add cookies
+const gbPlusBtn = document.querySelector("#add-gb");
+const ccPlusBtn = document.querySelector("#add-cc");
+const sugarPlusBtn = document.querySelector("#add-sugar");
+// selecting the element with an id of sub cookies
+const gbMinusBtn = document.querySelector("#minus-gb");
+const ccMinusBtn = document.querySelector("#minus-cc");
+const sugarMinusBtn = document.querySelector("#minus-sugar");
+
+//total cookies
+const total = document.querySelector("#qty-total");
 
 // Code to update name display
-credit.textContent = `Created by ${yourName}`
+credit.textContent = `Created by ${yourName}`;
 
-// Event listener for clicks on the "+" button for Gingerbread cookies
-gbPlusBtn.addEventListener('click', function() {
-// HINT: You can delete this console.log after you no longer need it!
-console.log('Gingerbread + button was clicked!')
+// Clickable cookie code that adds one
 
-// TODO: Write the code to be run when the "+" button for "Gingerbread" is clicked
-})
+let gbqty = document.querySelector("#qty-gb");
+gbPlusBtn.addEventListener("click", function () {
+  gb++;
+  gbqty.textContent = gb;
 
-// TODO: Hook up event listeners for the rest of the buttons
+  console.log("Gingerbread + button was clicked!");
+});
+
+let ccqty = document.querySelector("#qty-cc");
+ccPlusBtn.addEventListener("click", function () {
+  cc++;
+  ccqty.textContent = cc;
+  console.log("Chocolate + button was clicked!");
+});
+
+let sugarqty = document.querySelector("#qty-sugar");
+sugarPlusBtn.addEventListener("click", function () {
+  sugar++;
+  sugarqty.textContent = sugar;
+  console.log("Sugar + button was clicked!");
+});
+
+// clickable cookie code that subtracts one
+
+gbMinusBtn.addEventListener("click", function () {
+  gb--;
+  gbqty.textContent = gb;
+  console.log("Gingerbread + button was clicked!");
+});
+
+ccMinusBtn.addEventListener("click", function () {
+  cc--;
+  ccqty.textContent = cc;
+  console.log("Chocolate + button was clicked!");
+});
+
+sugarMinusBtn.addEventListener("click", function () {
+  sugar--;
+  sugarqty.textContent = sugar;
+  console.log("Sugar + button was clicked!");
+});
+
+
+
+
+
+
+
